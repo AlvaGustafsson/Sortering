fe0562a21e888f76da308e07809d678be7a1b2a4
Alva Gustafsson <algu01002@utb.vaxjo.se>
2020-05-16 15:37:17 +0200

    Sortering (bubble, insertion och merge)

---
+++ Sortering/Sortering/Class1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Sortering
+{
+    public class Class1
+    {
+        public double Bubblesort(List<int> bubble)
+        {
+            Stopwatch Bubblewatch = new Stopwatch();
+            Bubblewatch.Start();
+            for (int i = 0; i < bubble.Count - 1; i++)
+            {
+                for (int j = 0; j < bubble.Count - 1; j++) 
+                {
+                    if (bubble[j] > bubble[j + 1])
+                    {
+                        int x = bubble[j];
+                        bubble[j] = bubble[j + 1];
+                        bubble[j + 1] = x;
+                    }
+                }
+            }
+            Bubblewatch.Stop();
+            return Bubblewatch.ElapsedMilliseconds;
+        }
+        public double Insertionsort(List<int> insertion)
+        {
+            Stopwatch Insertionwatch = new Stopwatch();
+            Insertionwatch.Start();
+            for (int i = 1; i < insertion.Count; i++)
+            {
+                int j = i;
+                while (j > 0 && insertion[j - 1] > insertion[j])
+                {
+                    int x = insertion[j];
+                    insertion[j] = insertion[j - 1];
+                    insertion[j - 1] = x;
+                    j--;
+                }
+            }
+            Insertionwatch.Stop();
+            return Insertionwatch.ElapsedMilliseconds;
+        }
+        public double StopwatchMerge(List<int> merge)
+        {
+            Stopwatch Mergewatch = new Stopwatch();
+            Mergewatch.Start();
+            List<int> Sorted = Mergesort(merge);
+            Mergewatch.Stop();
+            return Mergewatch.ElapsedMilliseconds;
+        }
+        private List<int> Mergesort(List<int> merge)
+        {
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+            if (merge.Count <= 1)
+            {
+                return merge;
+            }
+
+            int x = merge.Count / 2;
+            left = merge.GetRange(0, x);
+            right = merge.GetRange(x, merge.Count - x);
+            left = Mergesort(left);
+            right = Mergesort(right);
+            return Merge(left, right);
+        }
+        private List<int> Merge(List<int> left, List<int> right)
+        {
+            List<int> result = new List<int>();
+            while (left.Count > 0 && right.Count > 0)
+            {
+                if (left[0] <= right[0])
+                {
+                    result.Add(left[0]);
+                    left = left.GetRange(1, left.Count - 1);
+                }
+                else
+                {
+                    result.Add(right[0]);
+                    right = right.GetRange(1, right.Count - 1);
+                }
+            }
+            if (left.Count > 0)
+            {
+                result.AddRange(left);
+            }
+            else
+            {
+                result.AddRange(right);
+            }
+            return result;
+        }
+    }
+}
---
+++ Sortering/Sortering/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+namespace Sortering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 Sorting = new Class1();
+            List<int> list = new List<int>();
+            Random number = new Random();
+            for (int i = 0; i < 10000; i++)
+            {
+                int nr = number.Next(1, 10001);
+                list.Add(nr);
+            }
+            List<int> bubblesort = new List<int>(list);
+            List<int> insertionsort = new List<int>(list);
+            List<int> mergesort = new List<int>(list);
+
+            double bubbletime = Sorting.Bubblesort(bubblesort);
+            Console.WriteLine("Bubbletime = " + bubbletime + " ms");
+            double insertiontime = Sorting.Insertionsort(insertionsort);
+            Console.WriteLine("Insertiontime = " + insertiontime + " ms");
+            double mergetime = Sorting.StopwatchMerge(mergesort);
+            Console.WriteLine("Mergetime = " + mergetime + " ms");
+        }
+    }
+}
